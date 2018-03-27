f7def125b71cd329e2049afa0e668e7ca962862e
Ben Marwick <benmarwick@gmail.com>
2018-03-27 15:07:01 -0700

    add here() to the source() etc. to uncomplicate file paths

--- Analysis/Graphics/permillgraphfunc.R
+++ Analysis/Graphics/permillgraphfunc.R
@@ -1,22 +1,44 @@
-#source("Analysis/Tables/permillcalculation.R")
+# source("Analysis/Tables/permillcalculation.R")
 
-permillgraph <- function(state, all){
-  
-  if(all){
-    matplot(2000:2017, t(fepermill[,3:20]), type="l", 
-            col= "grey", xlab = "Year", ylab = "Total Killings/Million", main = "Total Police Killings in US by State")
-  
-  
-    lines(2000:2017, fepermill[fepermill$state_name=="United States",3:20], 
-            col="black")
-  
-    lines(2000:2017, fepermill[fepermill$state_name==state,3:20], 
-            col= "red")
-  
-    legend("topright", legend = c(state, "United States", "Other States"), lty = c(1,1), lwd = c(2.5,2.5), 
-            col = c("red", "black", "grey"), text.col = "black", bty = "n", cex = 0.75)
-  }else{
-    plot(2000:2017, fepermill[fepermill$state_name==state,3:20], type="l", col= "red", xlab = "Year", ylab = "Total Killings/Million", main = paste("Total Police Killings in",state,sep = " "))
+
+permillgraph <- function(state, all) {
+  if (all) {
+    matplot(
+      2000:2017,
+      t(fepermill[, 3:20]),
+      type = "l",
+      col = "grey",
+      xlab = "Year",
+      ylab = "Total Killings/Million",
+      main = "Total Police Killings in US by State"
+    )
+    
+    
+    lines(2000:2017, fepermill[fepermill$state_name == "United States", 3:20],
+          col = "black")
+    
+    lines(2000:2017, fepermill[fepermill$state_name == state, 3:20],
+          col = "red")
+    
+    legend(
+      "topright",
+      legend = c(state, "United States", "Other States"),
+      lty = c(1, 1),
+      lwd = c(2.5, 2.5),
+      col = c("red", "black", "grey"),
+      text.col = "black",
+      bty = "n",
+      cex = 0.75
+    )
+  } else{
+    plot(
+      2000:2017,
+      fepermill[fepermill$state_name == state, 3:20],
+      type = "l",
+      col = "red",
+      xlab = "Year",
+      ylab = "Total Killings/Million",
+      main = paste("Total Police Killings in", state, sep = " ")
+    )
   }
 }
-
--- Analysis/Tables/permillcalculation.R
+++ Analysis/Tables/permillcalculation.R
@@ -3,22 +3,26 @@ library(tidyr)
 library(rio)
 
 #---------------------------------read in datasets-----------------------------
-load("Data/Final/Pop.Rdata")
-fatalencounters <- rio::import("https://docs.google.com/spreadsheets/d/1dKmaV_JiWcG8XBoRgP8b4e9Eopkpgt7FL7nyspvzAsE/edit#gid=0")
+load(here::here("Data/Final/Pop.Rdata"))
+fatalencounters <-
+  rio::import(
+    "https://docs.google.com/spreadsheets/d/1dKmaV_JiWcG8XBoRgP8b4e9Eopkpgt7FL7nyspvzAsE/edit#gid=0"
+  )
 #eventually this will be Jainul's compiled work :)
 
 
 #------------------------------censusdata---------------------------------------------------
-reorder_pop_state <- state_pops[order(state_pops$state_abb),]
-pop_state_and_us <- rbind(reorder_pop_state, all_pops[1,])
-pop_state_and_us_mill <- pop_state_and_us[,3:20]/1000000
+reorder_pop_state <- state_pops[order(state_pops$state_abb), ]
+pop_state_and_us <- rbind(reorder_pop_state, all_pops[1, ])
+pop_state_and_us_mill <- pop_state_and_us[, 3:20] / 1000000
 
 
 #-----------------------------organize fatal encounters data-------------------------------------
 
 #Move variable names from first row to column headers
-colnames(fatalencounters) <- fatalencounters[1,]
-fatalencounters <- tail(fatalencounters, -1) #remove row with column names
+colnames(fatalencounters) <- fatalencounters[1, ]
+fatalencounters <-
+  tail(fatalencounters, -1) #remove row with column names
 #rename fe colnames to be shorter/match with census data
 colnames(fatalencounters)[27] <- "Year"
 colnames(fatalencounters)[10] <- "State"
@@ -28,30 +32,30 @@ colnames(fatalencounters)[28] <- "?"
 
 ##create table of state by year counts
 fedata <- fatalencounters %>%
-  group_by(Year)%>%
-  count(State)%>%
+  group_by(Year) %>%
+  count(State) %>%
   spread(Year, n)
 
 #change NA values (where no data matched) to 0
 fedata[is.na(fedata)] <- 0
 
 #Calculate US total
-fedata["Total" ,][,-1] <- colSums(fedata[,-1])
+fedata["Total" , ][, -1] <- colSums(fedata[, -1])
 fedata[is.na(fedata)] <- "US"
 
 #remove extra column/row (hopefully this won't be an issue once I have jainul's) and remove 2018
-fedataedit <- fedata[2:53,c(1, 3:20)]
+fedataedit <- fedata[2:53, c(1, 3:20)]
 
 #add full state names
-fedataedit <- cbind(pop_state_and_us[,1], fedataedit)
+fedataedit <- cbind(pop_state_and_us[, 1], fedataedit)
 
-  
+
 #------------------------------calculate deaths per million------------------------------
 
 #calculate deaths per million population
-fepermill <- fedataedit[,-c(1:2)] / pop_state_and_us_mill
+fepermill <- fedataedit[, -c(1:2)] / pop_state_and_us_mill
 
 #find avegages over all years
-fepermill <- cbind(fedataedit[,1:2], fepermill, rowMeans(fepermill))
+fepermill <- cbind(fedataedit[, 1:2], fepermill, rowMeans(fepermill))
 
-colnames(fepermill)[1]<- "state_name"
+colnames(fepermill)[1] <- "state_name"
