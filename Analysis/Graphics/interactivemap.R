35a5e2fb3488b043655886593d6a2788fc81524d
Maddi Cummins <maddic9@uw.edu>
2018-04-18 18:07:59 -0700

    When clicked points now include: Name, city, date & description

--- Analysis/Graphics/interactivemap.R
+++ Analysis/Graphics/interactivemap.R
@@ -3,6 +3,6 @@ library(leaflet)
 interactivemap <- leaflet(data = fatalencounters, width = "100%") %>% addTiles() %>%
   addMarkers( ~ as.double(fatalencounters$Longitude),
               ~ as.double(fatalencounters$Latitude),
-              popup = ~ fatalencounters$`Location of death (city)`,
+              popup = ~ paste(fatalencounters$`Subject's name`, fatalencounters$`Location of death (city)`, fatalencounters$`Date&Description`, sep = ", "),
               label = ~ fatalencounters$`Subject's name`,
               clusterOptions = markerClusterOptions())
