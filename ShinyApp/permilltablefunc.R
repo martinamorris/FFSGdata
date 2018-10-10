c0d979f179ee4a8a503efe6379e4c48ee9b1170e
Maddi Cummins <maddic9@uw.edu>
2018-10-10 10:21:58 -0700

    bug fix - function wasn't working when all was FALSE

--- ShinyApp/permilltablefunc.R
+++ ShinyApp/permilltablefunc.R
@@ -5,29 +5,32 @@ library(dplyr)
 #' 
 #' Creates a table of fatal encounters by state
 #' 
-#' @param state a state name, the state that is displayed or highlighted
+#' @param statenm a state name, the state that is displayed or highlighted
 #' @param all a boolean, if TRUE table includes all states, if FALSE table displays just the given state
 #' @param capita a boolean, if TRUE evaluates cases per million population of each state, if FALSE just calculates total cases
 #' 
 #' @seealso \code{\link{permillcalc}} and \code{\link{permilltable}}
 #' 
 #' @export
-permilltable <- function(state, all, capita){
+permilltable <- function(statenm, all, capita){
+  
   rounded <- permillcalc(capita = capita)
+  
   if(capita){
     rounded[,3:21] <- round(rounded[,3:21], 2)
   }
+  
   if(all){
     datatable(rounded) %>% formatStyle(
       'state_name',
       target = 'row',
       backgroundColor = styleEqual(
-        state, "yellow"
+        statenm, "yellow"
       )
     )
   }else{
     df <- rounded %>%
-      filter(state_name == state)
+      filter(state_name == statenm)
     datatable(df)
   }
   
