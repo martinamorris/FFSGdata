89363c743c1a75ca6caf06ea826f879df0826cc8
Vaughn Johnson <vneck@uw.edu>
2019-04-09 12:47:02 -0700

    removed old files, and removed output from demo

--- Data/Missingess.R
+++
@@ -1,14 +0,0 @@
----
-title: "Vignette Title"
-author: "Vignette Author"
-date: "`r Sys.Date()`"
-output: rmarkdown::html_vignette
-vignette: >
-  %\VignetteIndexEntry{Vignette Title}
-  %\VignetteEngine{knitr::rmarkdown}
-  \usepackage[utf8]{inputenc}
----
-
-# Hello World
-
-This is a vignette
