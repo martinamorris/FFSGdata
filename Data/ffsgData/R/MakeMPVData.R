0792567bd30d875331769165c799b5c97d252427
Vaughn Johnson <vneck@uw.edu>
2019-04-09 14:15:23 -0700

    added 'wb' to xlsx dowload in mpv

--- Data/ffsgData/R/MakeMPVData.R
+++ Data/ffsgData/R/MakeMPVData.R
@@ -1,6 +1,6 @@
 #' ---
 #' title: "Scraping data from mappingpoliceviolence.net"
-#' author: "Jainul Vaghasia"
+#' author: "Vaughn Johnson"
 #' date: '`r format(Sys.Date(), "%d %B %Y")`'
 #' ---
 
@@ -10,19 +10,20 @@ library(dplyr)
 library(here)
 
 #' Scrape the MPV database
-#' 
+#'
 #' @param url URL for MPV spreadsheet
 #' @param save_file filename to be saved in /ScrapedFiles/
 #' @return Void. Saves the data to `save_file`.
 #' @export
 scrape_MPV_data <- function(url, save_file) {
-  TMP = "MPVTEMP"
-  # get xlsx file and write
-  download.file(url, destfile = TMP)
-  
-  # View
-  mpv <- read_xlsx(TMP)
-  file.remove(TMP)
-  
-  save(mpv, file=save_file)
+    TMP = "MPVTEMP"
+
+    # get xlsx file and write
+    download.file(url, destfile = TMP, mode='wb')
+
+    # View
+    mpv <- read_xlsx(TMP)
+    file.remove(TMP)
+
+    save(mpv, file=save_file)
 }
