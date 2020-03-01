2bed8efc56e97c72c45a905a9221594eaec66213
VaughnIsaacJohnson <vneck@uw.edu>
2020-03-01 14:11:03 -0800

    FE schema changed underneath us

--- Vaughn/ffsgData/R/Scraping/MakeFEData.R
+++ Vaughn/ffsgData/R/Scraping/MakeFEData.R
@@ -30,7 +30,7 @@ library(rio)
 #' @return Void. Saves the data to `save_file`.
 #' @export
 scrape_FE_data <- function(url, save_file) {
-  fe <- rio::import(url, sep=",")
+  fe <- rio::import(url)
 
   ##################################################################################
 
@@ -39,7 +39,6 @@ scrape_FE_data <- function(url, save_file) {
   irrelevant_cols = c("Unique ID",
                       "Unique identifier (redundant)",
                       "Unique ID formula",
-                      "Were police aware of symptoms of mental illness before interaction? INTERNAL USE, NOT FOR ANALYSIS",
                       "Video")
 
   fe = fe %>% select(-irrelevant_cols)
@@ -60,13 +59,13 @@ scrape_FE_data <- function(url, save_file) {
                    "Full Address" = 'fullAddress',
                    "Latitude" = 'latitude',
                    "Longitude" = 'longitude',
-                   "Agency(ies) involved in death" = 'agency',
-                    "Cause of death" = 'causeOfDeath',
-                    "A brief description of the circumstances surrounding the death" = 'circumstances',
-                   "Official disposition of death (justified or other) INTERNAL USE, NOT FOR ANALYSIS" = 'officialDisposition',
-            "Link to news article or photo of official document" = 'URLarticle',
-                    "Date&Description" = 'Description',
-                    "Date (Year)" = 'year')
+                   "Agency responsible for death" = 'agency',
+                   "Cause of death" = 'causeOfDeath',
+                   "A brief description of the circumstances surrounding the death" = 'circumstances',
+                   "Dispositions/Exclusions INTERNAL USE, NOT FOR ANALYSIS" = 'officialDisposition',
+                    "Link to news article or photo of official document" = 'URLarticle',
+                   "Date&Description" = 'Description',
+                   "Date (Year)" = 'year')
 
   # Dp
   fe = fe %>% plyr::rename(new_names)
